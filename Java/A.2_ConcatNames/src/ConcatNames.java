66bff057c7a33fdc9ef2a18bbad3ed6f1142ec09
MohanadSinan <mohanad14141@hotmail.com>
2021-03-27 12:33:18 +0300

    Add: A.2_ConcatNames Java

---
+++ Java/A.2_ConcatNames/src/ConcatNames.java
@@ -0,0 +1,15 @@
+public class ConcatNames {
+
+    public static String concatNames(String name1, String name2) {
+        // write your code here
+        return name1 + " " + name2;
+
+    }
+
+    public static void main(String[] args) {
+
+        String R = concatNames("Hello", "World!");
+        System.out.println(R);
+
+    }
+}
