fbecd8597d2dcc7ee350da558e98e7885940810e
MohanadSinan <mohanad14141@hotmail.com>
2021-03-27 11:48:50 +0300

    Add: A.1 Java

---
+++ A.1_sub/src/Sub.java
@@ -0,0 +1,43 @@
+public class Sub {
+
+    public static long sub(int a, int b) {
+        // write your code here
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("a\t-\tb\t=\toutput\n-----------------------");
+
+        var a = 10;
+        var b = 5;
+        long R1 = sub(a, b);
+        System.out.println(a + "\t-\t" + b + "\t=\t  " + R1);
+
+        a = 3;
+        b = -3;
+        long R2 = sub(a, b);
+        System.out.println(a + "\t-\t" + b + "\t=\t  " + R2);
+
+        a = -4;
+        b = 1;
+        long R3 = sub(a, b);
+        System.out.println(a + "\t-\t" + b + "\t=\t  " + R3);
+
+        a = 0;
+        b = -1;
+        long R4 = sub(a, b);
+        System.out.println(a + "\t-\t" + b + "\t=\t  " + R4);
+
+        a = 0;
+        b = 0;
+        long R5 = sub(a, b);
+        System.out.println(a + "\t-\t" + b + "\t=\t  " + R5);
+
+        a = -96;
+        b = -4;
+        long R6 = sub(a, b);
+        System.out.println(a + "\t-\t" + b + "\t=\t  " + R6);
+    }
+
+}
