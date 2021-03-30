583f97b115ae0eac5997dc5542d6d21b709b74fb
MohanadSinan <mohanad14141@hotmail.com>
2021-03-30 09:30:23 +0300

    Edit:

--- Java/A.2_ConcatNames/src/ConcatNames.java
+++ Java/A.2_ConcatNames/src/ConcatNames.java
@@ -3,7 +3,6 @@ public class ConcatNames {
     public static String concatNames(String name1, String name2) {
         // write your code here
         return name1 + " " + name2;
-
     }
 
     public static void main(String[] args) {
