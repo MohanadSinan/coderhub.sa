9faaa788d1a16bf0bfee4e2ce5d642a8e8160d4d
MohanadSinan <mohanad14141@hotmail.com>
2021-03-30 07:50:35 +0300

    Fix: A.2_ConcatNames

--- Java/A.2_ConcatNames/src/ConcatNames.java
+++ Java/A.2_ConcatNames/src/ConcatNames.java
@@ -8,8 +8,26 @@ public class ConcatNames {
 
     public static void main(String[] args) {
 
-        String R = concatNames("Hello", "World!");
-        System.out.println(R);
+        System.out.println("a\t\tb\t\t\t\toutput\n-----------------------------------");
 
+        var a = "Be";
+        var b = "yourself";
+        String R1 = concatNames(a, b);
+        System.out.println(a + "\t\t" + b + "\t  " + R1);
+
+        a = "Hello";
+        b = "World!";
+        String R2 = concatNames(a, b);
+        System.out.println(a + "\t" + b + "\t\t  " + R2);
+
+        a = "Coffee";
+        b = "shop";
+        String R3 = concatNames(a, b);
+        System.out.println(a + "\t" + b + "\t\t  " + R3);
+
+        a = "Good";
+        b = "morning";
+        String R4 = concatNames(a, b);
+        System.out.println(a + "\t" + b + "\t\t  " + R4);
     }
 }
